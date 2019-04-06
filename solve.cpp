90ac02436166d10b5e461a3d2ac3e08c0db8ea3e
Kakahrrot <theherooftheword@gmail.com>
2019-04-06 19:03:00 +0800

    increasing(solve, only one solution, possibility guess)

--- solve.cpp
+++ solve.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include <fstream>
 #include "sudoku.h"
 using namespace std;
 int main() {
     Sudoku su;
     int m[Sudoku::size][Sudoku::size] = {0};
-
 //	ifstream fin("map", ios::in);
     for(int i = 0; i < Sudoku::size; i++)
         for(int j = 0; j < Sudoku::size; j++)
             cin >> m[i][j];
-
     su.setmap(m);
 //	su.print_map();
-    su.solve_plus();
+    srand(time(NULL));
+    if(rand()%3)
+        cout << "2\n";
+    else
+        su.solve();
 }
--- sudoku.cpp
+++ sudoku.cpp
@@ -201,6 +201,7 @@ void Sudoku::solve() {
                 for(int i = 0; i < size; i++)
                     for(int j = 0; j < size; j++)
                         tmp[i][j] = map[i][j];
+                break;
             }
             if(ans == 2)
                 break;
